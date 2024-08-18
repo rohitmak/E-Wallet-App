a20decaa8f741a57d126d6bf7786101940cfa90b
Rohit Makwana <rmakwana060@gmail.com>
2024-08-18 21:09:53 +0530

    BE URL updated

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-// const backendURL = "https://e-wallet-app-backend-plum.vercel.app/api/v1";
-const backendURL = "http://localhost:3000/api/v1";
+const backendURL = "https://e-wallet-app-backend-plum.vercel.app/api/v1";
+// const backendURL = "http://localhost:3000/api/v1";
 export default backendURL;
