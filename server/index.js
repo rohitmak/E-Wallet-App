ce5959b849ea069f1f4b389ed183a09e50ea6b95
Rohit Makwana <rmakwana060@gmail.com>
2024-08-18 21:35:46 +0530

    CORS Error Updated

--- server/index.js
+++ server/index.js
@@ -6,6 +6,17 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 
+// MIDDLEWARES
+app.use(
+  cors({
+    origin: `${process.env.CORS_ORIGIN}`,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
 // DATABASE
 import connectDB from "./db/db.js";
 
@@ -22,11 +33,6 @@ connectDB()
   .catch((error) => {
     console.log(`MONGO DB CONNECTION FAILED!! `, error);
   });
-
-// MIDDLEWARES
-app.use(cors());
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 // ROUTERS
 import userRouter from "./routes/user.routes.js";
